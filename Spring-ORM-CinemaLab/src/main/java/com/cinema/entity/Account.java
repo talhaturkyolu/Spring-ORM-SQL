bbb324a5be520045954d91cedec2254c51cf8ea5
Talha Turkyolu <turkyolutalha@gmail.com>
2020-12-26 15:37:08 -0600

    Cinema App

---
+++ Spring-ORM-CinemaLab/src/main/java/com/cinema/entity/Account.java
@@ -0,0 +1,33 @@
+package com.cinema.entity;
+
+import com.cinema.enums.UserRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "account_details")
+public class Account extends BaseEntity{
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @OneToOne(mappedBy = "account")
+    private User user;
+
+
+}
